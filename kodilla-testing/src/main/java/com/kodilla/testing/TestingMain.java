56f6da9cff0b55d873fd3ab907106b2d5a536f0c
davv1d <kowalskidawid63@gmail.com>
2019-04-11 23:13:32 +0200

    Fourth commit

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,7 +1,31 @@
 package com.kodilla.testing;
 
+import com.kodilla.testing.calculator.Calculator;
+import com.kodilla.testing.user.SimpleUser;
+
 public class TestingMain {
     public static void main(String[] args) {
-        System.out.println("Modul 6: Wprowadzenie do testowania oprogramowania");
+        String name = "name";
+        SimpleUser simpleUser = new SimpleUser(name);
+        String resultName = simpleUser.getUsername();
+        if (resultName.equals(name)) {
+            System.out.println("test ok");
+        } else {
+            System.out.println("Error");
+        }
+        System.out.println("Test - pierwszy test jednostkowy:");
+        Calculator calculator = new Calculator();
+        int resultAdd = calculator.add(5, 2);
+        if (resultAdd == 7) {
+            System.out.println("Add test OK");
+        } else {
+            System.out.println("Add ERROR");
+        }
+        int resultSubstract = calculator.substract(10, 5);
+        if (resultSubstract == 5) {
+            System.out.println("Substract test OK");
+        } else {
+            System.out.println("Substract test ERROR");
+        }
     }
 }
