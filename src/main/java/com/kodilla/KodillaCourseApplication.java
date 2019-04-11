443bfb847464403d3b4bdfcc7712fae396ba900b
davv1d <kowalskidawid63@gmail.com>
2019-04-11 22:18:47 +0200

    Third commit

--- src/main/java/com/kodilla/KodillaCourseApplication.java
+++ src/main/java/com/kodilla/KodillaCourseApplication.java
@@ -8,7 +8,8 @@ public class KodillaCourseApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(KodillaCourseApplication.class, args);
-        System.out.println("uuu");
+
+
 
     }
 
