09ceed0131390eb563b015a90a6751d667b95e09
davv1d <kowalskidawid63@gmail.com>
2019-04-17 19:58:14 +0200

    Eighth commit

--- kodilla-testing/src/main/java/com/kodilla/testing/shape/ShapeCollector.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/shape/ShapeCollector.java
@@ -6,7 +6,7 @@ public class ShapeCollector {
     private ArrayList<Shape> figures = new ArrayList<Shape>();
 
     public int getFiguresQuantity(){
-        return 100;//figures.size();
+        return figures.size();
     }
 
     public void addFigure(Shape theShape) {
@@ -14,7 +14,7 @@ public class ShapeCollector {
     }
 
     public boolean removeFigure(Shape theShape) {
-        boolean result = true;
+        boolean result = false;
         if (figures.contains(theShape)) {
             figures.remove(theShape);
             result = true;
