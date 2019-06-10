6672d1f24d16d889cf2a416c3aaf126e98003a5d
davv1d <kowalskidawid63@gmail.com>
2019-06-10 21:44:19 +0200

    Exercise 14.1

---
+++ kodilla-spring-intro/src/main/java/com/kodilla/spring/intro/shape/Figure.java
@@ -0,0 +1,8 @@
+package com.kodilla.spring.intro.shape;
+
+public class Figure {
+    public String draw(){
+        System.out.println("This is a geometric figure");
+        return null;
+    }
+}
---
+++ kodilla-spring/src/main/java/com/kodilla/spring/Circle.java
@@ -0,0 +1,12 @@
+package com.kodilla.spring;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Circle  implements Shape {
+
+    @Override
+    public String getShapeName() {
+        return "This is a circle";
+    }
+}
