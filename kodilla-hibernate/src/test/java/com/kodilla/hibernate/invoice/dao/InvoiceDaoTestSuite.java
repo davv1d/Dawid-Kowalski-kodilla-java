c74533446ce2dc57166a534aca76bc51bf17a76a
davv1d <kowalskidawid63@gmail.com>
2019-07-02 21:55:53 +0200

    Exercise 17.3

---
+++ kodilla-hibernate/src/test/java/com/kodilla/hibernate/invoice/dao/InvoiceDaoTestSuite.java
@@ -0,0 +1,52 @@
+package com.kodilla.hibernate.invoice.dao;
+
+import com.kodilla.hibernate.invoice.Invoice;
+import com.kodilla.hibernate.invoice.Item;
+import com.kodilla.hibernate.invoice.Product;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import javax.validation.constraints.NotNull;
+import java.math.BigDecimal;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class InvoiceDaoTestSuite {
+
+    @Autowired
+    private InvoiceDao invoiceDao;
+
+    @Test
+    public void testInvoiceDaoSave() {
+        //Given
+        Product apple = new Product("Apple");
+        Product orange = new Product("Orange");
+
+        Item item1 = new Item(new BigDecimal(20), 5);
+        item1.setProduct(apple);
+
+        Item item2 = new Item(new BigDecimal(10), 10);
+        item2.setProduct(orange);
+
+        Invoice invoice = new Invoice("01/2019");
+        invoice.getItems().add(item1);
+        invoice.getItems().add(item2);
+
+        item1.setInvoice(invoice);
+        item2.setInvoice(invoice);
+
+        //When
+        invoiceDao.save(invoice);
+        int id = invoice.getId();
+
+        //Then
+        Assert.assertNotEquals(0, id);
+
+        //CleanUp
+        invoiceDao.deleteById(id);
+    }
+}
--- kodilla-hibernate/src/test/java/com/kodilla/hibernate/taskList/dao/TaskListDaoTestSuite.java
+++ kodilla-hibernate/src/test/java/com/kodilla/hibernate/taskList/dao/TaskListDaoTestSuite.java
@@ -1,5 +1,7 @@
 package com.kodilla.hibernate.taskList.dao;
 
+import com.kodilla.hibernate.task.Task;
+import com.kodilla.hibernate.task.TaskFinancialDetails;
 import com.kodilla.hibernate.taskList.TaskList;
 import org.junit.Assert;
 import org.junit.Test;
@@ -8,6 +10,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import javax.validation.constraints.NotNull;
+import java.math.BigDecimal;
 import java.util.List;
 
 @RunWith(SpringRunner.class)
@@ -30,6 +34,32 @@ public class TaskListDaoTestSuite {
         Assert.assertEquals(1, readTaskLists.size());
         //CleanUp
         int id = readTaskLists.get(0).getId();
+        taskListDao.deleteById(id);
+    }
+
+    @Test
+    public void testTaskListDaoSaveWithTasks() {
+        //Given
+        Task task1 = new Task("Test: Learn Hibernate", 14);
+        Task task2 = new Task("Test: Write some entities", 3);
+        TaskFinancialDetails tfd1 = new TaskFinancialDetails(new BigDecimal(20), false);
+        TaskFinancialDetails tfd2 = new TaskFinancialDetails(new BigDecimal(10), false);
+        task1.setTaskFinancialDetails(tfd1);
+        task2.setTaskFinancialDetails(tfd2);
+        TaskList taskList = new TaskList(LIST_NAME, "ToDo tasks");
+        taskList.getTasks().add(task1);
+        taskList.getTasks().add(task2);
+        task1.setTaskList(taskList);
+        task2.setTaskList(taskList);
+
+        //When
+        taskListDao.save(taskList);
+        int id = taskList.getId();
+
+        //Then
+        Assert.assertNotEquals(0, id);
+
+        //CleanUp
         taskListDao.deleteById(id);
     }
 
